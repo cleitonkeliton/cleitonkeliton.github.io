d75d583f516ef4a7b7403685f126bf1b2fc29a89
cleitonkeliton <155588167+cleitonkeliton@users.noreply.github.com>
2024-12-10 22:52:13 -0300

    Add files via upload
    Portfólio pessoal

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Bem-vindo ao meu portfólio!");
+const toggleButton = document.getElementById('trocar-tema');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+});
+const btnCurriculo = document.getElementById('toggle-curriculo');
+const curriculoSection = document.getElementById('curriculo-section');
+
+btnCurriculo.addEventListener('click', () => {
+  if (curriculoSection.style.display === 'none' || curriculoSection.style.display === '') {
+    curriculoSection.style.display = 'block';
+    console.log('Currículo exibido');
+  } else {
+    curriculoSection.style.display = 'none';
+    console.log('Currículo ocultado');
+  }
+});
+
+const downloadButton = document.getElementById('download-curriculo');
+
+downloadButton.addEventListener('click', () => {
+  window.location.href = 'CleitonKelitondaSilvaValdevino.png';
+  console.log('Download do currículo iniciado');
+});
+
+
+console.log("Tema alternado!");
